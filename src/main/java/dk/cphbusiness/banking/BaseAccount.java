bef4833a6f10d6b45275acb882a249be2ca4b5a3
Martin Vestergaard <mrv@cphbusiness.dk>
2020-09-15 16:45:08 +0200

    Committed the changes made in class, with a few changes. Be aware that it's probably not a good idea to have the SmsService inside the BankAccount class (it's probably outside of its responsibility), but for demo purposes it made sense. The good thing is, that since we used Inversion of Control, the account only got the /interface/, and it doesn't know about the actual implementation. So it's easy to move the sendSms call to a more suitable class.

--- src/main/java/dk/cphbusiness/banking/BaseAccount.java
+++ src/main/java/dk/cphbusiness/banking/BaseAccount.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BaseAccount {
+  private SmsService smsService;
   private final Bank bank;
   private final Customer customer;
   private final String number;
   // private long balance = 0L;
   private final List<Transaction> transactions = new ArrayList<>();
 
-  public BaseAccount(Bank bank, Customer customer, String number) {
+  public BaseAccount(SmsService smsService, Bank bank, Customer customer, String number) {
+    this.smsService = smsService;
     this.bank = bank;
     this.customer = customer;
     this.number = number;
@@ -47,10 +49,17 @@ public class BaseAccount {
     }
 
   public void transfer(long amount, String destinationNumber) {
-    transfer(amount, bank.getAccount(destinationNumber));
-    // balance -= amount;
-    // BaseAccount destination = bank.getAccount(destinationNumber);
-    // destination.balance += amount;
+    var destinationAccount = bank.getAccount(destinationNumber);
+    if (destinationAccount == null) {
+      throw new NonExistingAccount();
+    }
+//    if (getBalance() < amount) {
+//      var success = smsService.SendSms(customer, "You have insufficient funds for this transfer :(");
+//      if (!success)
+//        throw new SmsFaultException();
+//      return;
+//    }
+    transfer(amount, destinationAccount);
     }
 
   public List<Transaction> getTransactions() {
---
+++ src/main/java/dk/cphbusiness/banking/SmsServiceImpl.java
@@ -0,0 +1,8 @@
+package dk.cphbusiness.banking;
+
+public class SmsServiceImpl implements SmsService {
+    @Override
+    public boolean SendSms(Customer customer, String text) {
+        return false;
+    }
+}
